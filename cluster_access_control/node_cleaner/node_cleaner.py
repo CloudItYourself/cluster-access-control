dd5efbeed93dbc6a9f22648a2ac7bed66fa24038
ronen4822 <ronen4822@gmail.com>
2024-01-30 13:49:23 +0200

    -should be golden now

--- cluster_access_control/node_cleaner/node_cleaner.py
+++ cluster_access_control/node_cleaner/node_cleaner.py
@@ -8,24 +8,30 @@ from kubernetes import client
 from kubernetes.client import V1Eviction
 from redis import Redis
 from pottery import RedisDict, Redlock
-from threading import Lock
 from cluster_access_control.utilities.environment import ClusterAccessConfiguration
 
 
 class NodeCleaner:
     NODE_CLEANING_DICT_NAME: Final[str] = "node_keepalive_dict"
     NODE_NAME_TO_ID: Final[str] = "node_name_to_current_id"
+    NODE_CLEANING_LOCK_NAME: Final[str] = "cluster-access-cleanup-lock"
     NODE_TIMEOUT_IN_SECONDS: Final[float] = 3
 
     def __init__(self):
         self._environment = ClusterAccessConfiguration()
         self._redis_client = Redis.from_url(f"{self._environment.get_redis_url()}/0")
+
+        self._redlock = Redlock(
+            key=self.NODE_CLEANING_LOCK_NAME, masters={self._redis_client}, auto_release_time = 100
+        )
+
         self._keepalive_nodes_dict = RedisDict(
             redis=self._redis_client, name=self.NODE_CLEANING_DICT_NAME
         )
         self._node_name_to_id = RedisDict(
             redis=self._redis_client, name=self.NODE_NAME_TO_ID
         )
+
         self._thread_pool = ThreadPoolExecutor()
         kubernetes.config.load_kube_config(
             config_file=self._environment.get_kubernetes_config_file()
@@ -48,40 +54,39 @@ class NodeCleaner:
             time.sleep(self.NODE_TIMEOUT_IN_SECONDS)
             try:
                 nodes = stale_node_deletion_client.list_node().items
+                with self._redlock:
+                    for node in nodes:
+                        if "ciy.persistent_node" not in node.metadata.labels:
+                            node_name = node.metadata.name
+                            node_has_id = node_name in self._node_name_to_id
 
-                for node in nodes:
-                    if "ciy.persistent_node" not in node.metadata.labels:
-                        node_name = node.metadata.name
-                        if node_name not in self._node_name_to_id and node_name not in grace_period_nodes:
-                            grace_period_nodes[node_name] = datetime.now()
-                            continue
+                            if node_has_id:
+                                node_keepalive_sent = self._node_name_to_id[node_name] in self._keepalive_nodes_dict
+                                node_id = self._node_name_to_id[node_name]
+                            else:
+                                node_id = None
+                                node_keepalive_sent = False
 
-                        elif node_name not in self._node_name_to_id and node_name in grace_period_nodes:
-                            if (datetime.now() - grace_period_nodes[node_name]).seconds > 120:
-                                print("Deleting node due to grace period expiry")
-                                self._thread_pool.submit(
-                                    self.clean_up_node,
-                                    node.metadata.name,
-                                    node.status.conditions[-1].type == "Ready",
-                                )
-                                grace_period_nodes.pop(node_name)
-                            continue
+                            if not node_has_id or not node_keepalive_sent:
+                                if node_name not in grace_period_nodes:
+                                    grace_period_nodes[node_name] = datetime.now()
+                                elif (datetime.now() - grace_period_nodes[node_name]).seconds > 60:
+                                    print("Deleting node due to grace period expiry")
+                                    self._thread_pool.submit(
+                                        self.clean_up_node,
+                                        node.metadata.name,
+                                        node.status.conditions[-1].type == "Ready",
+                                    )
+                                    grace_period_nodes.pop(node_name)
 
-                        node_keepalive_name = self._node_name_to_id[node_name]
-                        node_exists = node_keepalive_name in self._keepalive_nodes_dict
+                                    self._node_name_to_id.pop(node_name)
+                                    if node_has_id:
+                                        self._keepalive_nodes_dict.pop(node_id)
 
-                        if not node_exists and node_name not in grace_period_nodes:
-                            grace_period_nodes[node_name] = datetime.now()
-                        elif node_name in grace_period_nodes and (
-                                datetime.now() - grace_period_nodes[node_name]).seconds < 30:
-                            pass
-                        else:
-                            latest_keepalive = self._keepalive_nodes_dict[node_keepalive_name]
-                            if (
-                                    not node_exists
-                                    or (datetime.utcnow().timestamp() - latest_keepalive) > self.NODE_TIMEOUT_IN_SECONDS
-                            ):
-                                self._keepalive_nodes_dict.pop(node_keepalive_name, None)
+                                continue
+
+                            node_latest_keepalive = self._keepalive_nodes_dict[node_id]
+                            if (datetime.utcnow().timestamp() - node_latest_keepalive) > self.NODE_TIMEOUT_IN_SECONDS:
                                 print("Deleting node due to periodic message not received")
 
                                 self._thread_pool.submit(
@@ -90,6 +95,9 @@ class NodeCleaner:
                                     node.status.conditions[-1].type == "Ready",
                                 )
                                 grace_period_nodes.pop(node_name)
+                                self._node_name_to_id.pop(node_name)
+                                self._keepalive_nodes_dict.pop(node_id)
+
             except Exception as e:  # TODO IMPROVE ME
                 print(f"Failed to clean up nodes.. error: {e}")
 
