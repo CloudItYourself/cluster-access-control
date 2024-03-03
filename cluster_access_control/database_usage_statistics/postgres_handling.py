bd40ed02f22587a84860aaff708c9546b90f1dc8
ronen4822 <ronen4822@gmail.com>
2024-03-03 22:12:27 +0200

    -add pooling

--- cluster_access_control/database_usage_statistics/postgres_handling.py
+++ cluster_access_control/database_usage_statistics/postgres_handling.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from typing import Final, Optional
+from typing import Final
 
 import psycopg2
 import psycopg2.extras
 from psycopg2 import sql
+from psycopg2 import pool
 
 from cluster_access_control.utilities.environment import ClusterAccessConfiguration
 
@@ -17,20 +18,27 @@ class PostgresHandler:
 
     def __init__(self):
         self._postgres_details = ClusterAccessConfiguration().get_postgres_details()
-        self._connection: Optional[psycopg2.extensions.connection] = None
-        self._cursor: Optional[psycopg2.extensions.cursor] = None
         self._create_database_if_doesnt_exist()
+
+        self._connection_pool = psycopg2.pool.ThreadedConnectionPool(
+            5,  # Minimum number of connections
+            20,  # Maximum number of connections
+            user=self._postgres_details.user,
+            password=self._postgres_details.password,
+            host=self._postgres_details.host,
+            port=PostgresHandler.DB_PORT,
+            database=PostgresHandler.DB_NAME
+        )
+
         self._initialize_databases()
 
     def _create_database_if_doesnt_exist(self):
         try:
             # Attempt to connect to the desired database
-            self._connection = psycopg2.connect(dbname=PostgresHandler.DB_NAME, user=self._postgres_details.user,
-                                                password=self._postgres_details.password,
-                                                host=self._postgres_details.host,
-                                                port=PostgresHandler.DB_PORT)
-            self._connection.autocommit = True
-            self._cursor = self._connection.cursor()
+            connection = psycopg2.connect(dbname=PostgresHandler.DB_NAME, user=self._postgres_details.user,
+                                          password=self._postgres_details.password,
+                                          host=self._postgres_details.host,
+                                          port=PostgresHandler.DB_PORT)
         except psycopg2.Error as e:
             # If the connection fails, check if the database does not exist
             if 'database "{}" does not exist'.format(PostgresHandler.DB_NAME) in str(e):
@@ -48,14 +56,6 @@ class PostgresHandler:
                 # Close the cursor and connection to the default database
                 cursor.close()
                 connection.close()
-
-                # Now, reconnect to the newly created database
-                self._connection = psycopg2.connect(dbname=PostgresHandler.DB_NAME, user=self._postgres_details.user,
-                                                    password=self._postgres_details.password,
-                                                    host=self._postgres_details.host,
-                                                    port=PostgresHandler.DB_PORT)
-                self._connection.autocommit = True
-                self._cursor = self._connection.cursor()
             else:
                 raise RuntimeError(
                     "Error: Could not connect to PostgreSQL. Please check your credentials and database settings.")
@@ -69,61 +69,90 @@ class PostgresHandler:
             registration_time timestamp,
             abrupt_disconnects INTEGER
         );
-        DO $$ BEGIN
-            CREATE TYPE DAY_OF_WEEK AS ENUM ('Sunday', 'Monday', 'Tuesday',
-                        'Wednesday', 'Thursday', 'Friday', 'Saturday');
-        EXCEPTION
-            WHEN duplicate_object THEN null;
-        END $$;
-
-        CREATE TABLE IF NOT EXISTS {PostgresHandler.NODE_USAGE_TABLE} (
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(100),
-            day_of_week DAY_OF_WEEK,
-            second_since_midnight INTEGER,
-            check_in_count INTEGER
-        );
         """
-        # Execute the SQL command
-        self._cursor.execute(create_table_query)
+        try:
+            conn = self._connection_pool.getconn()
+            conn.autocommit = True
+            with conn.cursor() as cur:
+                # Execute the SQL command
+                cur.execute(create_table_query)
+        finally:
+            self._connection_pool.putconn(conn)
 
     def _node_already_registered(self, nodes_name: str) -> bool:
-        self._cursor.execute(
-            f"SELECT EXISTS(SELECT 1 FROM {PostgresHandler.NODE_DETAILS_NAME} WHERE name='{nodes_name}')")
-        return self._cursor.fetchone()[0]
+        try:
+            conn = self._connection_pool.getconn()
+            with conn.cursor() as cur:
+                cur.execute(
+                    f"SELECT EXISTS(SELECT 1 FROM {PostgresHandler.NODE_DETAILS_NAME} WHERE name='{nodes_name}')")
+                return cur.fetchone()[0]
+        finally:
+            self._connection_pool.putconn(conn)
 
     def register_node(self, node_name: str) -> bool:
         if self._node_already_registered(node_name):
             return True
         table_init = []
-        for day_in_week in ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']:
-            for i in range(0, PostgresHandler.SECONDS_IN_DAY):
-                table_init.append((node_name, day_in_week, i, '0'))
-
-        # Start a transaction
-        self._cursor.execute("BEGIN;")
+        for i in range(7):
+            for j in range(0, PostgresHandler.SECONDS_IN_DAY // 10):
+                table_init.append((i, j, '0'))
         try:
-            # Register node query
-            register_node_query = sql.SQL("""
-            DO $$ BEGIN
-               INSERT INTO {}
-               VALUES (DEFAULT, %s, to_timestamp(%s), 0);
-            END $$;
-            """).format(sql.Identifier(PostgresHandler.NODE_DETAILS_NAME))
-            self._cursor.execute(register_node_query, (node_name, datetime.utcnow().timestamp()))
+            conn = self._connection_pool.getconn()
+            with conn.cursor() as cur:
+                cur.execute("BEGIN;")
+                try:
+                    # Register node query
+                    register_node_query = sql.SQL(f"""
+                    DO $$ BEGIN
+                        CREATE TABLE IF NOT EXISTS {PostgresHandler.NODE_USAGE_TABLE}_{node_name} (
+                            id SERIAL PRIMARY KEY,
+                            day_of_week smallint,
+                            seconds_since_midnight_divided smallint,
+                            check_in_count INTEGER
+                        );
+                       INSERT INTO {PostgresHandler.NODE_DETAILS_NAME}
+                       VALUES (DEFAULT, %s, to_timestamp(%s), 0);
+                       CREATE INDEX idx_seconds_since_midnight_divided ON {PostgresHandler.NODE_USAGE_TABLE}_{node_name} (seconds_since_midnight_divided);
+                    END $$;
+                    """)
+                    cur.execute(register_node_query, (node_name, datetime.utcnow().timestamp()))
 
-            insert_query = f"insert into {PostgresHandler.NODE_USAGE_TABLE} values %s"
-            psycopg2.extras.execute_values(self._cursor, insert_query, table_init, template="(DEFAULT, %s, %s,%s,%s)",
-                                           page_size=100)
+                    insert_query = f"insert into {PostgresHandler.NODE_USAGE_TABLE}_{node_name} values %s"
+                    psycopg2.extras.execute_values(self._cursor, insert_query, table_init,
+                                                   template="(DEFAULT, %s, %s, %s)",
+                                                   page_size=100)
 
-            # Commit the transaction
-            self._cursor.execute("COMMIT;")
-        except Exception as e:
-            # Rollback the transaction in case of any error
-            self._cursor.execute("ROLLBACK;")
-            print("Error occurred:", e)
+                    # Commit the transaction
+                    cur.execute("COMMIT;")
+                    return True
+                except Exception as e:
+                    # Rollback the transaction in case of any error
+                    cur.execute("ROLLBACK;")
+                    print("Error occurred:", e)
+                    return False
+        finally:
+            self._connection_pool.putconn(conn)
+
+    def update_node(self, node_name: str) -> bool:
+        if not self._node_already_registered(node_name):
+            return False
+
+        current_time = datetime.utcnow()
+        seconds_since_midnight = (current_time - current_time.replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )).seconds // 10
+        try:
+            conn = self._connection_pool.getconn()
+            conn.autocommit = True
+            with conn.cursor() as cur:
+                cur.execute(f"UPDATE {PostgresHandler.NODE_USAGE_TABLE}_{node_name} SET check_in_count=check_in_count + 1"
+                            f" WHERE seconds_since_midnight_divided={seconds_since_midnight} and day_of_week={current_time.weekday()}")
+        finally:
+            self._connection_pool.putconn(conn)
 
 
 if __name__ == '__main__':
     xd = PostgresHandler()
     xd.register_node("Ronen2")
+    for i in range(100000):
+        xd.update_node("Ronen2")
