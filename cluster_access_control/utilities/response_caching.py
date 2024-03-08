289d9ee12a1a2b8b7dd17522db1b220fff653ec4
ronen4822 <ronen4822@gmail.com>
2024-03-08 08:08:30 +0200

    -JESUS

--- cluster_access_control/utilities/response_caching.py
+++ cluster_access_control/utilities/response_caching.py
@@ -1,12 +1,10 @@
-import aioredis
 from fastapi_cache import FastAPICache
+from redis.asyncio import Redis
 from fastapi_cache.backends.redis import RedisBackend
-from redis.client import Redis
-
 from cluster_access_control.utilities.environment import ClusterAccessConfiguration
 
 
 async def initialize_fastapi_cache():
     environment = ClusterAccessConfiguration()
-    redis_client = aioredis.from_url(f"{environment.get_redis_url()}/0")
+    redis_client = Redis.from_url(f"{environment.get_redis_url()}/0")
     FastAPICache.init(RedisBackend(redis_client), prefix="cluster-access-control-fastapi-cache")
